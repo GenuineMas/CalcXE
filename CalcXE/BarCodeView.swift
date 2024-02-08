20973aa366dd443f0aaee322066dfb4d8bdf5b92
GenuineMas <MasalitinJW@gmail.com>
2024-02-09 01:23:49 +0200

    web request and tab view -add web view and web message handler - add bottom tab bar for split view tto web handler (future bar code) and bread units view -refactoring

---
+++ CalcXE/BarCodeView.swift
@@ -0,0 +1,18 @@
+//
+//  BarCodeView.swift
+//  CalcXE
+//
+//  Created by Genuine on 08.02.2024.
+//
+
+import SwiftUI
+
+struct BarCodeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BarCodeView()
+}
